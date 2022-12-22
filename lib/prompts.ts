2b3d22ce42508e739127e7208ca64c813768badd
chee-chyuan <60590919+chee-chyuan@users.noreply.github.com>
2022-12-22 21:53:28 +0800

    use path module instead of raw \

--- lib/prompts.ts
+++ lib/prompts.ts
@@ -1,184 +1,210 @@
-import inquirer from 'inquirer'
-import { compilerVersions, licenses, NETWORK, networkNames } from './constants.js'
-import { validateCairoFileName, validateCompilerVersion, validateContractOrClassAddress, validateContractOrClassAddressExistence, validateLicense, validateNetwork, validateNonEmptyString } from './validators.js'
-import { extractCairoContractName, extractFilesForVerification, extractNileForVerification, extractProtostarForVerification, normalizeContractOrClassAddress, withSpinner } from './utils.js'
-import chalk from 'chalk'
-import fuzzyPath from "inquirer-fuzzy-path"
+import inquirer from "inquirer";
+import {
+  compilerVersions,
+  licenses,
+  NETWORK,
+  networkNames,
+} from "./constants.js";
+import {
+  validateCairoFileName,
+  validateCompilerVersion,
+  validateContractOrClassAddress,
+  validateContractOrClassAddressExistence,
+  validateLicense,
+  validateNetwork,
+  validateNonEmptyString,
+} from "./validators.js";
+import {
+  extractCairoContractName,
+  extractFilesForVerification,
+  extractNileForVerification,
+  extractProtostarForVerification,
+  normalizeContractOrClassAddress,
+  withSpinner,
+} from "./utils.js";
+import chalk from "chalk";
+import fuzzyPath from "inquirer-fuzzy-path";
 
-inquirer.registerPrompt("fuzzypath", fuzzyPath)
+inquirer.registerPrompt("fuzzypath", fuzzyPath);
 
 export const enterNetwork = async () => {
-  const prompt = await inquirer.prompt(
-      {
-          name: 'network',
-          type: 'list',
-          choices: networkNames,
-          message: 'Choose network that you wish to verify on:',
-          validate: validateNetwork
-        }
-  )
+  const prompt = await inquirer.prompt({
+    name: "network",
+    type: "list",
+    choices: networkNames,
+    message: "Choose network that you wish to verify on:",
+    validate: validateNetwork,
+  });
 
-  return prompt.network
-}
+  return prompt.network;
+};
 
 export const enterClassOrContractAddress = async (network: NETWORK) => {
-  const prompt = await inquirer.prompt(
-        {
-            name: 'address',
-            type: 'input',
-            message: 'Enter the contract/class address on that network that you want to verify:',
-            validate: validateContractOrClassAddress
-          }
-    )
+  const prompt = await inquirer.prompt({
+    name: "address",
+    type: "input",
+    message:
+      "Enter the contract/class address on that network that you want to verify:",
+    validate: validateContractOrClassAddress,
+  });
 
-  return prompt.address
-}
+  return prompt.address;
+};
 
 export const enterExistingClassOrContractAddress = async (network: NETWORK) => {
   while (true) {
-    const address = await enterClassOrContractAddress(network)
+    const address = await enterClassOrContractAddress(network);
 
-    const doesTheAddressExist = await withSpinner('Verifying the address..', validateContractOrClassAddressExistence(network, address))
+    const doesTheAddressExist = await withSpinner(
+      "Verifying the address..",
+      validateContractOrClassAddressExistence(network, address)
+    );
 
     if (doesTheAddressExist) {
-      return normalizeContractOrClassAddress(address)
+      return normalizeContractOrClassAddress(address);
     }
 
-    console.log(chalk.red('The address entered does not exist on the network.'))
+    console.log(
+      chalk.red("The address entered does not exist on the network.")
+    );
   }
-}
+};
 
 export const enterCompilerVersion = async () => {
-  const prompt = await inquirer.prompt(
-      {
-          name: 'version',
-          type: 'list',
-          choices: compilerVersions,
-          message: 'Choose the compiler version that you wish to use:',
-          validate: validateCompilerVersion
-        }
-  )
+  const prompt = await inquirer.prompt({
+    name: "version",
+    type: "list",
+    choices: compilerVersions,
+    message: "Choose the compiler version that you wish to use:",
+    validate: validateCompilerVersion,
+  });
 
-  return prompt.version
-}
+  return prompt.version;
+};
 
 export const enterLicense = async () => {
-  const prompt = await inquirer.prompt(
-      {
-          name: 'license',
-          type: 'list',
-          choices: licenses,
-          message: 'Choose the license you wish to verify under:',
-          validate: validateLicense
-        }
-  )
+  const prompt = await inquirer.prompt({
+    name: "license",
+    type: "list",
+    choices: licenses,
+    message: "Choose the license you wish to verify under:",
+    validate: validateLicense,
+  });
 
-  return prompt.license
-}
+  return prompt.license;
+};
 
 export const enterIsAccountContract = async () => {
-  const prompt = await inquirer.prompt(
-      {
-          name: 'isAccount',
-          type: 'confirm',
-          message: 'Is this an account contract/class?'
-      }
-  )
+  const prompt = await inquirer.prompt({
+    name: "isAccount",
+    type: "confirm",
+    message: "Is this an account contract/class?",
+  });
 
-  return prompt.isAccount
-}
+  return prompt.isAccount;
+};
 
 export const enterContractToVerify = async () => {
   const prompt = await inquirer.prompt([
     {
-      type: 'fuzzypath',
-      name: 'contract',
-      excludePath: nodePath => nodePath.startsWith('node_modules'),
-      excludeFilter: nodePath => !nodePath.endsWith(".cairo"),
-      itemType: 'file',
-      message: 'The contract/class to verify:',
+      type: "fuzzypath",
+      name: "contract",
+      excludePath: (nodePath) => nodePath.startsWith("node_modules"),
+      excludeFilter: (nodePath) => !nodePath.endsWith(".cairo"),
+      itemType: "file",
+      message: "The contract/class to verify:",
       searchText: "Looking for the contract..",
       suggestOnly: false,
-      validate: (path) => validateCairoFileName(path.value)
-    }
-  ])
+      validate: (path) => validateCairoFileName(path.value),
+    },
+  ]);
 
-  return prompt.contract
-}
+  return prompt.contract;
+};
 
 export const enterContractToVerifyWithValidDependencies = async () => {
   while (true) {
-    const contractPath = await enterContractToVerify()
-
+    const contractPath = await enterContractToVerify();
     try {
       const nile = extractNileForVerification();
       if (nile) {
-        const files = await withSpinner('Checking for Nile dependencies..', new Promise((resolve, reject) => {
-          const { files, notFound } = extractFilesForVerification(nile)
-          if (notFound.length) {
-            reject(notFound)
-          } else {
-            resolve(files)
-          }
-        }))
-    
+        const files = await withSpinner(
+          "Checking for Nile dependencies..",
+          new Promise((resolve, reject) => {
+            const { files, notFound } = extractFilesForVerification(nile);
+            if (notFound.length) {
+              reject(notFound);
+            } else {
+              resolve(files);
+            }
+          })
+        );
+
         if (files) {
-          return { contract: contractPath, files }
+          return { contract: contractPath, files };
         }
       }
     } catch (err) {
-      throw new Error(`Nile was not resolved! Reason: ${err}`)
+      throw new Error(`Nile was not resolved! Reason: ${err}`);
     }
 
     try {
       const protostar = extractProtostarForVerification();
       if (protostar) {
-        const files = await withSpinner('Checking for Protostar dependencies..', new Promise((resolve, reject) => {
-          const resolvedPaths = protostar.map(path => extractFilesForVerification(path))
-          const notFound = resolvedPaths.flatMap(resolved => resolved.notFound)
-          if (notFound.length) {
-            reject(notFound)
-          } else {
-            resolve(resolvedPaths.flatMap(resolved => resolved.files))
-          }
-        }))
-    
+        const files = await withSpinner(
+          "Checking for Protostar dependencies..",
+          new Promise((resolve, reject) => {
+            const resolvedPaths = protostar.map((path) =>
+              extractFilesForVerification(path)
+            );
+            const notFound = resolvedPaths.flatMap(
+              (resolved) => resolved.notFound
+            );
+            if (notFound.length) {
+              reject(notFound);
+            } else {
+              resolve(resolvedPaths.flatMap((resolved) => resolved.files));
+            }
+          })
+        );
+
         if (files) {
-          return { contract: contractPath, files }
+          return { contract: contractPath, files };
         }
       }
     } catch (err) {
-      throw new Error(`Protostar was not resolved! Reason: ${err}`)
+      throw new Error(`Protostar was not resolved! Reason: ${err}`);
     }
 
-    const files = await withSpinner('Finding dependencies..', new Promise((resolve, reject) => {
-      const { files, notFound } = extractFilesForVerification(contractPath)
-      if (notFound.length) {
-        reject(notFound)
-      } else {
-        resolve(files)
-      }
-    }))
+    const files = await withSpinner(
+      "Finding dependencies..",
+      new Promise((resolve, reject) => {
+        const { files, notFound } = extractFilesForVerification(contractPath);
+        if (notFound.length) {
+          reject(notFound);
+        } else {
+          resolve(files);
+        }
+      })
+    );
 
     if (files) {
-      return { contract: contractPath, files }
+      return { contract: contractPath, files };
     }
 
-    console.log(chalk.red('There was a problem resolving dependencies...'))
-    return { contract: contractPath, files: [] }
+    console.log(chalk.red("There was a problem resolving dependencies..."));
+    return { contract: contractPath, files: [] };
   }
-}
+};
 
 export const enterContractName = async (selectedContractName: string) => {
-  const prompt = await inquirer.prompt(
-      {
-          name: 'contractName',
-          type: 'input',
-          default: extractCairoContractName(selectedContractName),
-          message: 'Enter contract/class name:',
-          validate: validateNonEmptyString
-      }
-  )
+  const prompt = await inquirer.prompt({
+    name: "contractName",
+    type: "input",
+    default: extractCairoContractName(selectedContractName),
+    message: "Enter contract/class name:",
+    validate: validateNonEmptyString,
+  });
 
-  return prompt.contractName
-}
+  return prompt.contractName;
+};
